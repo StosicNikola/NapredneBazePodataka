75b528fed70e1c927540ef002313af36879a476b
StosicNikola <stosic.s.nikola@gmail.com>
2023-01-05 15:53:36 +0100

    new back

--- naprednebazeback/Startup.cs
+++ naprednebazeback/Startup.cs
@@ -13,41 +13,39 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Neo4jClient;
 
-namespace NapredneBazeProjekat
+namespace naprednebazeback
 {
     public class Startup
     {
-       
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-             
-            
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public async void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
+
+            //Neo4J connection
+
             var neo4JClient = new BoltGraphClient(new Uri("bolt://localhost:7687"), "neo4j", "root1234");
             neo4JClient.ConnectAsync();
-            
-          
             services.AddSingleton<IGraphClient>(neo4JClient);
-
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NapredneBazeProjekat", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "naprednebazeback", Version = "v1" });
             });
+
+            //Logger
             ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
             {
                 builder.AddConsole();
                 builder.AddDebug();
             });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,7 +55,7 @@ namespace NapredneBazeProjekat
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NapredneBazeProjekat v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "naprednebazeback v1"));
             }
 
             app.UseHttpsRedirection();

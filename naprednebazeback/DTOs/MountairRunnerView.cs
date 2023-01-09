beb7f367a842d313d2df3551c2302995902bce0b
StosicNikola <stosic.s.nikola@gmail.com>
2023-01-09 18:26:27 +0100

    Leaderboard redis

--- naprednebazeback/DTOs/MountairRunnerView.cs
+++ naprednebazeback/DTOs/MountairRunnerView.cs
@@ -3,20 +3,27 @@ using naprednebazeback.ObjectModel;
 
 namespace naprednebazeback.DTOs
 {
-    public class MountairRunnerView
+    public class MountainRunnerView
     {
         public long PersonId {get; set;}
         public string Name {get; set;}
-        public int Score {get; set;}
+        public double Score {get; set;}
         public DateTime TimeStampRunner {get; set;}
 
-        public MountairRunnerView(){
+        public MountainRunnerView(){
             
         }
 
-        public MountairRunnerView(Person person, int score, DateTime time){
+        public MountainRunnerView(Person person, double score, DateTime time){
             PersonId = person.Id;
             Name  = person.name;
+            Score = score;
+            TimeStampRunner = time;
+        }
+
+        public MountainRunnerView(long id,string name, double score, DateTime time){
+            PersonId = id;
+            Name  = name;
             Score = score;
             TimeStampRunner = time;
         }
--- naprednebazeback/RedisDataLayer/RedisDao.cs
+++ naprednebazeback/RedisDataLayer/RedisDao.cs
@@ -3,38 +3,116 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using naprednebazeback.DTOs;
+using Microsoft.Extensions.Logging;
 
 namespace naprednebazeback.RedisDataLayer
 {
     public class RedisDao
     {
-         readonly string RedisLeaderboard = "Leaderboard";
+         private ILogger _logger;
+         readonly string MainLeaderboardForAllPerson = "MainLeaderboard";
+         public Dictionary<long,string> PersonOwnScoreForAllTime ;
          readonly RedisClient redis = new RedisClient(RedisConfig.SingleHost);
 
-         public RedisDao(){
-           // redis.PushItemToList("myKey","Nikola");
+         public RedisDao(ILogger logger){
+            PersonOwnScoreForAllTime = UploadLeaderboardNamesFromRedis();   
+            _logger = logger;       
          }
 
-         public void AddRunnerToSet(String name,DateTime time,long score){
-            string blub = ConvertToBlub(name,time);
-            redis.AddItemToSortedSet(RedisLeaderboard,blub,score);
+         public void AddRunnerToSet(MountainRunnerView mrv){
+            string blub = ConvertToBlubForMain(mrv.PersonId,mrv.Name,mrv.TimeStampRunner);
+            redis.AddItemToSortedSet(MainLeaderboardForAllPerson,blub,mrv.Score);
+            string nameOfLeaderboard ;
+            if (PersonOwnScoreForAllTime.TryGetValue(mrv.PersonId, out nameOfLeaderboard)){
+              string newBlub = ConvertToBlubForPeople(mrv.TimeStampRunner);
+              redis.AddItemToSortedSet(nameOfLeaderboard,newBlub,mrv.Score);
+            }
+            else{
+              CreateNewLeaderboardForNewPerson(mrv);
+              if (PersonOwnScoreForAllTime.TryGetValue(mrv.PersonId, out nameOfLeaderboard)){
+                 string newBlub = ConvertToBlubForPeople(mrv.TimeStampRunner);
+                 redis.AddItemToSortedSet(nameOfLeaderboard,newBlub,mrv.Score);
+              }
+            }
+         }
+
+         public List<MountainRunnerView> GetMainLeaderboardFrom0toNPeople(int numOfReturnItems){
+            IDictionary<string,double> items = GetItemWithScoreForMainLeaderboard(numOfReturnItems);
+            List<MountainRunnerView> mrvs  = new List<MountainRunnerView>();
+            foreach(KeyValuePair<string,double> item in items){
+              string[] subs = item.Key.Split("#");
+              long id = (long)Convert.ToDouble(subs[0]);
+              mrvs.Add( new MountainRunnerView(id,subs[1],item.Value, Convert.ToDateTime(subs[2])));
+            }
             
-         }
-
-         public string GetFirstLeader(){
-            List<string> record = GetAllItemsFromSortedSet();
-            string[] subs = record.FirstOrDefault().Split("#");
-
-            return record.FirstOrDefault();
+            return mrvs;
          }
 
          public List<string> GetAllItemsFromSortedSet(){
-          return redis.GetAllItemsFromSortedSet(RedisLeaderboard);
+          return redis.GetAllItemsFromSortedSet(MainLeaderboardForAllPerson);
          }
 
-         public string ConvertToBlub(string name, DateTime time){
-          string blub = name + "#" + time.ToString("dd/MM/yyyy HH:mm:ss");
+         public string ConvertToBlubForMain(long id,string name, DateTime time){
+          string blub = id + "#"+ name + "#" + time.ToString("dd/MM/yyyy HH:mm:ss");
           return blub;
+         }
+
+         public string ConvertToBlubForPeople(DateTime time){
+         // string blub = id + "#"+ name + "#" + time.ToString("dd/MM/yyyy HH:mm:ss");
+          string blub = time.ToString("dd/MM/yyyy HH:mm:ss");
+          return blub;
+         }
+
+
+         public IDictionary<string,double> GetItemWithScoreForMainLeaderboard(int numOfReturnItems){
+           return redis.GetRangeWithScoresFromSortedSet(MainLeaderboardForAllPerson,0,numOfReturnItems);
+         }
+
+         public void CreateNewLeaderboardForNewPerson(MountainRunnerView mrv){
+              string Value = "Leaderboard#"+mrv.PersonId+"#"+mrv.Name;
+              PersonOwnScoreForAllTime.Add(mrv.PersonId,Value);
+         }
+
+         public void DeleteAllItemInDb(){
+          redis.FlushAll();
+         }
+
+          public List<MountainRunnerView> GetLeaderboardForPersonFrom0toNitems(int numOfReturnItems, long personId){
+            IDictionary<string,double> items = GetItemWithScoreForPersonLeaderboard(numOfReturnItems,personId);
+            string name = this.PersonOwnScoreForAllTime[personId];
+            string[] subs = name.Split("#");
+            List<MountainRunnerView> mrvs  = new List<MountainRunnerView>();
+            foreach(KeyValuePair<string,double> item in items){
+              mrvs.Add( new MountainRunnerView(personId,subs[2],item.Value, Convert.ToDateTime(item.Key)));
+            }   
+            return mrvs;
+         }
+
+           
+           public IDictionary<string,double> GetItemWithScoreForPersonLeaderboard(int numOfReturnItems , long PersonId){
+            string nameOfLeaderboard;
+            if (PersonOwnScoreForAllTime.TryGetValue(PersonId, out nameOfLeaderboard)){
+              return redis.GetRangeWithScoresFromSortedSet(nameOfLeaderboard,0,numOfReturnItems);
+            }
+            return new Dictionary<string,double>();
+         }      
+
+         public Dictionary<long,string> GetDictionary(){
+          return this.PersonOwnScoreForAllTime;
+         }
+
+         public Dictionary<long,string> UploadLeaderboardNamesFromRedis(){
+            List<string> leaderboards = redis.GetAllKeys();
+            leaderboards.Remove(MainLeaderboardForAllPerson);
+             Dictionary<long,string> items = new Dictionary<long,string>();
+            foreach(string item in leaderboards){
+              string[] subs = item.Split("#");
+              long id = (long)Convert.ToDouble(subs[1]);
+              items.Add(id,item);
+            }           
+            return items;
+            
          }
     }
 }

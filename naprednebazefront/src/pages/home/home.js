da0714939373293c6841b1d24028de5d69f8d2d1
TamaraMilo <tamara.m.milovanovic@gmail.com>
2023-01-22 00:17:01 +0100

    .

--- naprednebazefront/src/pages/home/home.js
+++ naprednebazefront/src/pages/home/home.js
@@ -1,4 +1,3 @@
-
 import './home.css'
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from "react";
@@ -11,13 +10,23 @@ import Header from '../../components/header/header';
 function Home(){
 
     const[regions,setRegions]= useState([]);
+    const[hikes,setHikes]= useState([]);
 
     useEffect(()=>{
         if(regions.length===0)
         axios.get(`https://localhost:5001/Region`)
         .then(res=>setRegions(res.data[0]))
-        .catch(e=>alert(e))
-        console.log(regions)
+        .catch(e=>console.log(e))
+
+        if(hikes.length === 0)
+        {
+            axios.get(`https://localhost:5001/hike`)
+            .then(res=>
+                {
+                    setHikes(res.data[0])
+                })
+                .catch(e=>console.log(e))
+        }
 
     },regions);
 
@@ -25,9 +34,17 @@ function Home(){
     return(<>
     <Header />
     <div className='home-usponi'>
-    <h1 className='home-uspon'>Usponi</h1>
+    <h1 className='home-uspon'>Hikes</h1>
     
-        <Link to='uspon/Rtanj'>
+        { 
+            hikes.map(element=>{
+            return <Link to={`uspon/${element.name}`} >
+            <div className='home-card'>
+                <h3>{element.name}</h3>&emsp;<h3>Date: {element.date}</h3>
+            </div>
+            </Link>
+        })}
+        {/* <Link to='uspon/Rtanj'>
             <div className='home-card'>
                 <h3>RTANJ</h3>&emsp;<h3>Datum:13.12.2022.</h3>
             </div>
@@ -41,8 +58,10 @@ function Home(){
             <div className='home-card'>
                 <h3>MIDZOR</h3>&emsp;<h3>Datum:13.12.2022.</h3>
             </div>
-        </Link>
-    </div>
+    </Link>*/}
+    </div> 
+
+    
     <div className='regions'>
         <h1>Regions</h1>
         { regions.map(element => {
